09f1fc583871a909008f593a1e749ccec9c08531
Lun Wang <lun.wang@thetradedesk.com>
2023-08-16 10:30:12 +0800

    Add isReady to indicate provider readiness

--- src/main/java/com/uid2/enclave/IAttestationProvider.java
+++ src/main/java/com/uid2/enclave/IAttestationProvider.java
@@ -1,5 +1,8 @@
 package com.uid2.enclave;
 
 public interface IAttestationProvider {
+    // isReady indicates whether a provider is ready to serve getAttestationRequest
+    default boolean isReady() { return true; }
+
     byte[] getAttestationRequest(byte[] publicKey) throws AttestationException;
 }
